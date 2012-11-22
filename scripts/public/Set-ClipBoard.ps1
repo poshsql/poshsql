806d7065d785ef0f6c5288eff3cf80316f0a3f5b
Shiju <shiju.samuel.vendor@gmo.com>
2012-11-22 07:54:34 -0500

    changed fileformat to dos for many files

--- scripts/public/Set-ClipBoard.ps1
+++ scripts/public/Set-ClipBoard.ps1
@@ -1,13 +1,3 @@
-<#
-.SYNOPSIS
-   <A brief description of the script>
-.DESCRIPTION
-   <A detailed description of the script>
-.PARAMETER <paramName>
-   <Description of script parameter>
-.EXAMPLE
-   <An example of using the script>
-#>
 function Set-ClipBoard {
 	param(
 	$text
